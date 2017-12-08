16ee9c7f8c68ae4b15679ceefbbb43378203c669
Christian Nwamba <nwambachristian@gmail.com>
2017-12-08 22:08:18 +0100

    using cl video player

--- src/Post/index.js
+++ src/Post/index.js
@@ -1,55 +1,88 @@
-import React from 'react';
-import './Post.css';
+import React, { Component } from 'react';
 import cloudinary from 'cloudinary-core';
-import { Image, Transformation, Video } from 'cloudinary-react';
-var cl = cloudinary.Cloudinary.new({ cloud_name: 'christekh' });
+import 'cloudinary-video-player';
+import { Image, Transformation } from 'cloudinary-react';
 
-const isVideo = url => url.split('.')[url.split('.').length - 1] === 'mp4';
-const fetchPublicId = url =>
-  url.split('/')[url.split('/').length - 1].split('.')[0];
-const Post = ({ user: { nickname, avatar }, post: { image, caption } }) => (
-  <article className="Post">
-    <header>
-      <div className="Post-user">
-        <div className="Post-user-avatar">
-          <img src={avatar} alt={nickname} />
+import './Post.css';
+import '../../node_modules/cloudinary-video-player/dist/cld-video-player.min.css';
+
+class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vDom: null
+    };
+    this.cl = cloudinary.Cloudinary.new({ cloud_name: 'christekh' });
+    this.vDom = null;
+    this.vPlayer = null;
+  }
+
+  componentDidMount() {
+    if (this.vDom) {
+      this.vPlayer = this.cl.videoPlayer(this.vDom, {
+        transformation: {
+          width: 687,
+          height: 687,
+          crop: 'pad'
+        }
+      });
+      this.vPlayer.source(this.fetchPublicId(this.props.post.image));
+    }
+  }
+
+  componentWillReceiveProps;
+
+  isVideo = url => url.split('.')[url.split('.').length - 1] === 'mp4';
+
+  fetchPublicId = url =>
+    url.split('/')[url.split('/').length - 1].split('.')[0];
+
+  render() {
+    const {
+      user: { nickname, avatar },
+      post: { image, caption }
+    } = this.props;
+    return (
+      <article className="Post">
+        <header>
+          <div className="Post-user">
+            <div className="Post-user-avatar">
+              <img src={avatar} alt={nickname} />
+            </div>
+            <div className="Post-user-nickname">
+              <span>{nickname}</span>
+            </div>
+          </div>
+        </header>
+        <div className="Post-image">
+          <div className="Post-image-bg">
+            {this.isVideo(image) ? (
+              <video
+                controls
+                autoPlay
+                loop
+                id={this.vId}
+                className="cld-video-player"
+                ref={vDom => (this.vDom = vDom)}
+              />
+            ) : (
+              <Image publicId={this.fetchPublicId(image)}>
+                <Transformation
+                  width="687"
+                  height="687"
+                  background="black"
+                  crop="pad"
+                />
+              </Image>
+            )}
+          </div>
         </div>
-        <div className="Post-user-nickname">
-          <span>{nickname}</span>
+        <div className="Post-caption">
+          <strong>{nickname}</strong> {caption}
         </div>
-      </div>
-    </header>
-    <div className="Post-image">
-      <div className="Post-image-bg">
-        {isVideo(image) ? (
-          <video
-            controls
-            loop
-            src={cl
-              .url(fetchPublicId(image), {
-                width: 687,
-                height: 687,
-                crop: 'pad',
-                background: 'black'
-              })
-              .replace('image', 'video')}
-          />
-        ) : (
-          <Image publicId={fetchPublicId(image)}>
-            <Transformation
-              width="687"
-              height="687"
-              background="black"
-              crop="pad"
-            />
-          </Image>
-        )}
-      </div>
-    </div>
-    <div className="Post-caption">
-      <strong>{nickname}</strong> {caption}
-    </div>
-  </article>
-);
+      </article>
+    );
+  }
+}
 
 export default Post;
